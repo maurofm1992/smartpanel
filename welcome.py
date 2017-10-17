7faac3f7f18dfcba5cf6834dc6d13e860589074a
David <dgabay2015@fau.edu>
2017-10-16 20:58:52 -0400

    This is a working version all aspects working

--- welcome.py
+++ welcome.py
@@ -15,16 +15,35 @@
 import os
 from flask import Flask, jsonify
 #import dbte
-#import Data_transfer
 from flask import url_for, redirect
+# Block of code that connects and retrieves data from database
+from cloudant.client import Cloudant
+from cloudant.error import CloudantException
+from cloudant.result import Result, ResultByKey
+from flask import Flask,jsonify,json
+
+client = Cloudant("39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix",
+                  "48e26645f504209f85b4c44d74a4cb14bc0d059a22b361534b78f406a513f8ff",
+                  url="https://39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix:48e26645f504209f85b4c44d74a4cb14bc0d059a22b361534b78f406a513f8ff@39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix.cloudant.com")
+client.connect()
+
+end_point = '{0}/{1}'.format("https://39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix:48e26645f504209f85b4c44d74a4cb14bc0d059a22b361534b78f406a513f8ff@39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix.cloudant.com", "coolstuff" + "/_all_docs?")
+params = {'include_docs': 'true'}
+
+response = client.r_session.get(end_point,params=params)
+table22 =response.json()['rows'][0]['doc']['current']
+
+
+
+# code for routing begins
+
 app = Flask(__name__)
 
 @app.route('/')
 def Welcome():
-    return app.send_file('index.html')
+    # return app.send_file('static/index.html')
 
     return redirect(url_for('static', filename='index.html'))
-
 
 
 @app.route('/myapp')
