b629e2f96115c0520e6d06b8745dea58f1683411
David <dgabay2015@fau.edu>
2017-11-06 20:40:13 -0500

    create a create db funct

--- functions.py
+++ functions.py
@@ -5,8 +5,18 @@ import serial
 import string
 
 
+from cloudant.client import Cloudant
+
+client = Cloudant("39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix",
+                  "48e26645f504209f85b4c44d74a4cb14bc0d059a22b361534b78f406a513f8ff",
+                  url="https://39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix:48e26645f504209f85b4c44d74a4cb14bc0d059a22b361534b78f406a513f8ff@39a4348e-3ce1-40cd-b016-1f85569d409e-bluemix.cloudant.com")
+client.connect()
 
 
+def create_db (db_name):
+    myDatabase = client.create_database(db_name)
+
+    return myDatabase
 
 def getVolts ():
     # Get I2C bus
