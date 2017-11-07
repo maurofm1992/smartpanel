c3e9d529f355e319ab416f19fd40dcb4d15c5fee
David <dgabay2015@fau.edu>
2017-11-06 20:25:41 -0500

    df

--- functions.py
+++ functions.py
@@ -5,42 +5,43 @@ import serial
 import string
 
 
-# Get I2C bus
-bus = smbus.SMBus(1)
 
-# I2C address, 0x2A(42)
-# Command for reading device identification data
-# 0x6A(106), 0x02(2), 0x00(0),0x00(0), 0x00(0) 0x00(0), 0xFE(254)
-# Header byte-2, command-2, byte 3, 4, 5 and 6 are reserved, checksum
-command2 = [0x6A, 0x02, 0x00, 0x00, 0x00, 0x00, 0xFE]
-bus.write_i2c_block_data(0x2A, 0x92, command2)
-j = 10
-#time.sleep(0.5)
-
-# I2C address, 0x2A(42)
-# Read data back from 0x55(85), 3 bytes
-# Type of Sensor, Maximum Current, No. of Channels
-data = bus.read_i2c_block_data(0x2A, 0x55, 3)
-
-# Convert the data
-typeOfSensor = data[0]
-maxvolt = data[1]
-noOfChannel = data[2]
-# I2C address, 0x2A(42)
-# Command for reading voltage
-# 0x6A(106), 0x05(5), 0x01(1),0x02(2), 0x00(0), 0x00(0) 0x04(4)
-# Header byte-2, command-5, start channel-1, stop channel-12, byte 5 and 6 reserved, checksum
-command1 = [0x6A, 0x05, 0x01, 0x02, 0x00, 0x00, 0x04]
-bus.write_i2c_block_data(0x2A, 0x92, command1)
-#time.sleep(0.5)
-
-# I2C address, 0x2A(42)
-# Read data back from 0x55(85), No. of Channels * 3 bytes
-# current MSB1, current MSB, current LSB
-data1 = bus.read_i2c_block_data(0x2A, 0x55, 2*3)
 
 
 def getVolts ():
+    # Get I2C bus
+    bus = smbus.SMBus(1)
+
+    # I2C address, 0x2A(42)
+    # Command for reading device identification data
+    # 0x6A(106), 0x02(2), 0x00(0),0x00(0), 0x00(0) 0x00(0), 0xFE(254)
+    # Header byte-2, command-2, byte 3, 4, 5 and 6 are reserved, checksum
+    command2 = [0x6A, 0x02, 0x00, 0x00, 0x00, 0x00, 0xFE]
+    bus.write_i2c_block_data(0x2A, 0x92, command2)
+    j = 10
+    #time.sleep(0.5)
+
+    # I2C address, 0x2A(42)
+    # Read data back from 0x55(85), 3 bytes
+    # Type of Sensor, Maximum Current, No. of Channels
+    data = bus.read_i2c_block_data(0x2A, 0x55, 3)
+
+    # Convert the data
+    typeOfSensor = data[0]
+    maxvolt = data[1]
+    noOfChannel = data[2]
+    # I2C address, 0x2A(42)
+    # Command for reading voltage
+    # 0x6A(106), 0x05(5), 0x01(1),0x02(2), 0x00(0), 0x00(0) 0x04(4)
+    # Header byte-2, command-5, start channel-1, stop channel-12, byte 5 and 6 reserved, checksum
+    command1 = [0x6A, 0x05, 0x01, 0x02, 0x00, 0x00, 0x04]
+    bus.write_i2c_block_data(0x2A, 0x92, command1)
+    #time.sleep(0.5)
+
+    # I2C address, 0x2A(42)
+    # Read data back from 0x55(85), No. of Channels * 3 bytes
+    # current MSB1, current MSB, current LSB
+    data1 = bus.read_i2c_block_data(0x2A, 0x55, 2*3)
     volts = []
 # Convert the data
     for i in range(0, 2) :
